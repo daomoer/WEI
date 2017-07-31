dd531f76e3444c00e1c951e2b18230b5b5b733f3
daomoer <643934580@qq.com>
2017-07-31 17:02:37 +0800

    Update wei sdk

---
+++ WEI/WEI.framework/Headers/WEI.h
@@ -0,0 +1,18 @@
+//
+//  WEI.h
+//  WEI
+//
+//  Created by YY on 2017/7/28.
+//  Copyright © 2017年 上海云盾信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WEI.
+FOUNDATION_EXPORT double WEIVersionNumber;
+
+//! Project version string for WEI.
+FOUNDATION_EXPORT const unsigned char WEIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WEI/PublicHeader.h>
+#import <WEI/testEncryption.h>
---
+++ WEI/WEI.framework/Headers/testEncryption.h
@@ -0,0 +1,15 @@
+//
+//  testEncryption.h
+//  WEI
+//
+//  Created by YY on 2017/7/28.
+//  Copyright © 2017年 上海云盾信息技术有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testEncryption : NSObject
+
+-(void)testEncryptionString;
+
+@end
